62e558367cb38238f1ef194b5b48fbea7da73dd0
Magosányi Árpád <mag@magwas.rulez.org>
2012-05-19 13:57:16 +0200

    a bit better readable plots

--- burndown.plot
+++ burndown.plot
@@ -5,12 +5,14 @@ set key invert samplen 4 spacing 1 width 0 height 0
 set style histogram rowstacked title  offset character 0, 0, 0
 set datafile missing '-'
 set style data histograms
-set xtics border in scale 0,0 nomirror rotate by -45  offset character 0, 0, 0 
-set xtics  norangelimit font ",8"
-set xtics   ()
+#set xtics border in scale 0,0 nomirror rotate by -45  offset character 0, 0, 0 
+#set xtics  norangelimit font ",2"
+#set xtics   ()
+set timefmt "%Y-%m-%d"
+set xtics rotate by -90
 set title "Burndown" 
 set datafile separator ","
-set terminal png
+set terminal png size 1024,480 font arial 7
 set output "sprint.png"
 #set yrange [ 0.00000 : 7.00000e+06 ] noreverse nowriteback
 i = 5
--- burndown.sh
+++ burndown.sh
@@ -2,15 +2,15 @@
 set -xe
 
 rm -f stamps.csv changes.csv wi.db burndown.dat sprint.dat
-iconv -f utf-8 -t utf-8//IGNORE <webissues.xml |java -jar ~/lib/saxon9he.jar -xsl:wi2.xslt -s:-
+iconv -f utf-8 -t utf-8//IGNORE <webissues.xml |saxon9 -xsl:wi2.xslt -s:-
 cat wischema.sql |sqlite3 wi.db
 ./burndown.py
 echo "<html><head> <meta http-equiv=\"Content-Type\" content=\"text/xhtml;charset=UTF-8\"/> <title>Burndown</title> </head><body>">out/index.html
 sqlite3 wi.db "select distinct value_new from changes where attr_id=15" | while read
 do
 	echo $REPLY
-	echo "<h1>$REPLY</h1><img src=\"$REPLY.png\"/><br/>">>out/index.html
-	(head -1 burndown.dat;grep "$REPLY," burndown.dat)|head -30 >sprint.dat
+	echo "<h1>$REPLY</h1><a name=\"$REPLY\"/><img src=\"$REPLY.png\"/><br/>">>out/index.html
+	(head -1 burndown.dat;grep "$REPLY," burndown.dat) >sprint.dat
 	gnuplot burndown.plot
 	mv sprint.png out/"$REPLY".png
 done
